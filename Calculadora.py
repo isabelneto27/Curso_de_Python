556ff0b15c8db9079125e7df678c47698ec83d16
isabelneto27 <110185919+isabelneto27@users.noreply.github.com>
2023-07-12 12:29:58 +0100

    Nova Calculadora

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+salário=float(input("informe o seu salário"))
+if salário<128000:
+    percentagem=0.2
+    aumento=salário*percentagem
+    novo_salário=salário+aumento
+elif(salário>=128000 and salário<=500000):
+    percentagem=0.15
+    aumento= salário*percentagem
+    novo_salário+aumento
+elif salário>=500000 and salário<=1000000:
+    percentagem=0.1
+    aumento=salário*percentagem
+    novo_salário=salário+aumento
+else:
+    percentagem=0.05
+    aumento=salário*percentagem
+    novo_salário=salário+aumento
+print("salário antes do reajuste:",salário)
+print("percentagem:",percentagem)
+print("aumento a ser aplicado:",aumento)
+print("novo salário:",novo_salário)
